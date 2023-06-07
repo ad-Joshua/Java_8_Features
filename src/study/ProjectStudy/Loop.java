0910c22938c081eda20a7a6001a1081fea5d499c
joshua <joshua.swaroop@algodomain.com>
2023-06-07 13:59:27 +0530

    commit java 8

--- src/study/ProjectStudy/Loop.java
+++ src/study/ProjectStudy/Loop.java
@@ -6,11 +6,16 @@ public class Loop {
 
         int [] x = {2};  //try with 1 and 2 separately
         for (int i = 0; i<x.length; i++){
-            if(x[i]%1==0){
-                s1 += "true";
-            }
             if(x[i]%2==0){
+                s1 += "true";
+            }else{
+                s1 += "false";
+            }
+            if(x[i]%1==0){
                 s1 += "2true";
+            }
+            if(x[i]!=0){
+                s1 += "3true";
             }
             System.out.println(s1);
         }
