bd89bcbf5bc35e4cf483314056d4d491a985ec46
joshua <joshua.swaroop@algodomain.com>
2023-06-12 19:32:05 +0530

    commit java 8

---
+++ src/study/ProjectStudy/Static1.java
@@ -0,0 +1,13 @@
+package study.ProjectStudy;
+
+public class Static1 {
+    int x;
+    static int y;
+    public static void main(String[] args) {
+        Static1 s1 = new Static1();
+        s1.x=10;
+        y = 20;
+        System.out.println(s1.x+" "+ y);
+
+    }
+}
--- src/study/ProjectStudy/StudentClass.java
+++ src/study/ProjectStudy/StudentClass.java
@@ -4,5 +4,7 @@ public class StudentClass {
     public static void main(String[] args) {
         Student studentDetails = Student.STUDENT_DETAILS;
         System.out.println("id:"+studentDetails.getId()+ " Name:"+studentDetails.getName());
+
+
     }
 }
