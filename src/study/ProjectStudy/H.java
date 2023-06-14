4919ccb276a50e6f5c2d7778198b5c642f3f0f24
joshua <joshua.swaroop@algodomain.com>
2023-06-14 13:13:35 +0530

    commit java 8

---
+++ src/study/ProjectStudy/H.java
@@ -0,0 +1,15 @@
+package study.ProjectStudy;
+
+public class H {
+    public static void main(String[] args) {
+        int x = 0;
+        for(int i=1; i<=10; ++i){ //i++ or ++i
+            if(i%2==0){
+               ++x;    //x++ or ++x   both are similar
+
+            }
+           // System.out.println(x);
+        }
+        System.out.println(x);
+    }
+}
