9b95fcc0389c42295bf38bcebd1255acfc9612f3
joshua <joshua.swaroop@algodomain.com>
2023-06-15 13:06:37 +0530

    commit java 8

---
+++ src/study/ProjectStudy/K.java
@@ -0,0 +1,17 @@
+package study.ProjectStudy;
+
+public class K {
+    public static void main(String[] args) {
+        boolean x = false;
+        int y =1;
+        if (y==1){
+            x=true;
+        }else if (y==0 || y==2){
+            x=true;
+        }
+
+        if(x){
+            System.out.println("x is "+ x);
+        }
+    }
+}
